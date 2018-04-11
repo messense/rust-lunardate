9480523e5ef70f630e7667c26cf8aad8a023990d
messense <messense@icloud.com>
2018-04-11 14:14:03 +0800

    Update crate metadata

---
+++ README.md
@@ -0,0 +1,11 @@
+# rust-lunardate
+
+A Chinese Calendar Library in Rust
+
+## Limits
+
+This library can only deal with year from 1900 to 2049 (in Chinese calendar).
+
+## License
+
+This work is released under the MIT license. A copy of the license is provided in the [LICENSE](./LICENSE) file.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+// this file is derived from lunar project.
+//
+// lunar project:
+//   Copyright (C) 1988,1989,1991,1992,2001 Fung F. Lee and Ricky Yeung
+//   Licensed under GPLv2.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
 extern crate chrono;
 #[macro_use]
 extern crate lazy_static;
